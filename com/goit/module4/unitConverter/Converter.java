2b5a88dc30e06e42703655398b676b6ea8f40592
ik0000 <d0pam1n9@gmail.com>
2015-12-24 17:40:24 +0200

    deleted:    com/goit/module4/unitConverter/Celsius.java 	modified:   com/goit/module4/unitConverter/Converter.java 	deleted:    com/goit/module4/unitConverter/Farenheit.java 	modified:   com/goit/module4/unitConverter/Runner.java

--- com/goit/module4/unitConverter/Converter.java
+++ com/goit/module4/unitConverter/Converter.java
@@ -2,14 +2,11 @@ package com.goit.module4.unitConverter;
 
 public class Converter {
 
-    public static double convertUtil(Celsius c) {
-        // Celsius to Farenheit
-        double cel = c.getCelsius();
-        return cel * 5 / 9 + 32;
+    public static double convertUtilC2F(double c) {
+        return c * 5 / 9 + 32;
     }
-    public static double convertUtil(Farenheit f) {
-        // Farenheit to Celsius
-        double far = f.getFarenheit();
-        return ((far - 32) * 5 / 9);
+    public static double convertUtilF2C(double f) {
+        return ((f - 32) * 5 / 9);
     }
-}
+
+    }
