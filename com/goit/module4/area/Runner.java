fe0862d2d2d549ff0cd9816bc840d1b341abbe54
ik0000 <d0pam1n9@gmail.com>
2015-12-24 17:26:58 +0200

    deleted:    com/goit/module4/area/AreaSizeCalculatorUtilcalculateArea.java 	modified:   com/goit/module4/area/Runner.java 	com/goit/module4/area/GeoFigureUtil.java

--- com/goit/module4/area/Runner.java
+++ com/goit/module4/area/Runner.java
@@ -4,11 +4,11 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Circle Area:");
-        System.out.println(AreaSizeCalculatorUtilcalculateArea.areaCalc(new Circle(1)));
+        System.out.println(GeoFigureUtil.areaCalc(new Circle(1)));
         System.out.println("Rectangle Area:");
-        System.out.println(AreaSizeCalculatorUtilcalculateArea.areaCalc(new Rectangle(2,3)));
+        System.out.println(GeoFigureUtil.areaCalc(new Rectangle(2,3)));
         System.out.println("Triangle Area:");
-        System.out.println(AreaSizeCalculatorUtilcalculateArea.areaCalc(new Triangle(2,3,4)));
+        System.out.println(GeoFigureUtil.areaCalc(new Triangle(2,3,4)));
 
     }
 }
